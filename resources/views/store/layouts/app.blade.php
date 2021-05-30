dde1ffff153a45c954c2470b1dad556f36969660
MINOUJSS <68520002+MINOUJSS@users.noreply.github.com>
2021-05-30 19:44:51 +0100

    modift goole analytics code 1

--- resources/views/store/layouts/app.blade.php
+++ resources/views/store/layouts/app.blade.php
@@ -7,7 +7,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/png" href="{{url('store/img')}}/logo3.png">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
+	<!--Google Analytics Code here-->
+	@if(google_analitycs_code_is_avtive())
+	{{print_google_analytics_code() }}
+	@endif
 	<title>الساورة دليفري</title>
 
 	<!-- Google font -->
@@ -33,10 +36,7 @@
 	<link type="text/css" rel="stylesheet" href="{{url('store')}}/css/starrr.css" />
 	<!-- sweet alert link-->
     <script src="{{url('vendor')}}/sweetalert/sweetalert.all.js"></script>    		
-	<!--Google Analytics Code here-->
-	@if(google_analitycs_code_is_avtive())
-	{{print_google_analytics_code() }}
-	@endif
+	
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
