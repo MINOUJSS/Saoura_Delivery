68c576857a77d3813368879f823b736b38cef4e0
MINOUJSS <minoujss@gmail.com>
2021-08-01 19:07:35 +0100

    fix supplier migartion set email nullable

--- database/migrations/2020_12_01_181104_create_suppliers_table.php
+++ database/migrations/2020_12_01_181104_create_suppliers_table.php
@@ -16,7 +16,7 @@ class CreateSuppliersTable extends Migration
         Schema::create('suppliers', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->string('mobile');
             $table->text('address');
             $table->timestamps();
