97caf003f355bb778ad6c87c4d9661ee57f7816e
MINOUJSS <minoujss@gmail.com>
2021-09-05 13:23:07 +0100

    add ckeditor css file link to app template

--- resources/views/store/layouts/app.blade.php
+++ resources/views/store/layouts/app.blade.php
@@ -36,7 +36,9 @@
 	<link type="text/css" rel="stylesheet" href="{{url('store')}}/css/starrr.css" />
 	<!-- sweet alert link-->
     <script src="{{url('vendor')}}/sweetalert/sweetalert.all.js"></script>    		
-	
+	<!--ckeditor-->
+    <link type="text/css" rel="stylesheet" href="{{ asset('ckeditor/contents.css')}}"/>
+
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
