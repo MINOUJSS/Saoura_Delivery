5cfb0be1c2b756dc3dd2a59ee129116556d9a761
MINOUJSS <minoujss@gmail.com>
2021-10-19 13:01:39 +0100

    fix countdow of deals 7

--- resources/views/store/layouts/inc/product-page/product-body.blade.php
+++ resources/views/store/layouts/inc/product-page/product-body.blade.php
@@ -213,7 +213,10 @@
                 }else if(d==0 && h<10 && m>10 && s>10)
                 {
                     document.getElementById('product-countdown').innerHTML='<li><span>0'+h+' سا</span></li>'+'<li><span>'+m+' د</span></li>'+'<li><span>'+s+' ثا</span></li>'; 
-                }            
+                }else
+                {
+                    document.getElementById('product-countdown').innerHTML='<li><span>'+d+' ي</span></li>'+'<li><span>'+h+' سا</span></li>'+'<li><span>'+m+' د</span></li>'+'<li><span>'+s+' ثا</span></li>';  
+                }           
         }
         const  product_id=document.getElementById('product_id').value;                                                                             
                 setInterval('deal_count_down(product_id)',1000);
