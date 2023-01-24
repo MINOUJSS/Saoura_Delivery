28d51091c2c6674d83c25b85b7d59548f28e956f
MINOUJSS <minoujss@gmail.com>
2023-01-24 21:40:59 +0100

    change type of dropsheping url column to text

--- database/migrations/2023_01_24_203334_change_type_of_dropshepig_url_to_text.php
+++ database/migrations/2023_01_24_203334_change_type_of_dropshepig_url_to_text.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDropshipingUrlToProductTable extends Migration
+class ChangeTypeOfDropshepigUrlToText extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddDropshipingUrlToProductTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('dropsheping_url')->nullable()->after('dropsheping');
+            $table->text('dropsheping_url')->nullable()->change();
         });
     }
 
@@ -26,7 +26,7 @@ class AddDropshipingUrlToProductTable extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('dropsheping_url');
+            $table->string('dropsheping_url')->nullable()->change();
         });
     }
 }
